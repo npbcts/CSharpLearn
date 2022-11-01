085ef65a27ea03d1beb00090a22ab7ae53a1fe49
npbcts <auditx@hotmail.com>
2022-11-01 11:53:21 +0800

    更新目录

--- content.py
+++ content.py
@@ -7,7 +7,7 @@ import re
 import os
 
 
-section_dic = {1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六', 7: '七', 8: '八', 9:'九'}
+section_dic = {0:'前言', 1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六', 7: '七', 8: '八', 9:'九'}
 
 def save_text(text, filename, mode='a', encoding='utf-8'):
     with open(filename, mode=mode, encoding=encoding) as f:
@@ -19,20 +19,33 @@ def content(content_name):
     files.sort()
     index = 0
     session_index = 0
+    practic_folder = []
     for file in files:
         chapter_index = re.findall(r'\d', file)
         if chapter_index:
-            session_index_tmp = int(chapter_index[0])
-            if session_index != session_index_tmp:
-                session_index = session_index_tmp
-                save_text(f'\n\n### 第{section_dic.get(session_index)}部分\n', content_name)
-            index += 1
-            chapter_text = f"{index}. [{file.split('.')[0]}]({file})  \n"
-            save_text(chapter_text, content_name)
+            if file.startswith('csharp'):
+                session_index_tmp = int(chapter_index[0])
+                if session_index != session_index_tmp:
+                    session_index = session_index_tmp
+                    content_str = f'\n\n### 第{section_dic.get(session_index)}部分\n'
+                    save_text(content_str, content_name)
+                
+                index += 1
+                chapter_text = f"{index}. [{file.split('.')[0]}]({file})  \n"
+                save_text(chapter_text, content_name)
+            else:
+                practic_folder.append(file)
+        
+    # 添加练习程序文件夹目录
+    save_text('\n\n### 练习程序列表\n', content_name)
+    if practic_folder:
+        for index, file in enumerate(practic_folder):
+            save_text( f'{index+1}. [{file}]({file})\n', content_name)
+
 
 
 if __name__ == '__main__':
-    content_name = 'csharp笔记-0000目录.md'
+    content_name = 'csharp笔记-000目录.md'
     if os.path.exists(content_name):
         os.remove(content_name)
     save_text('## CSharpLearn笔记目录\n', content_name)
--- csharp笔记-000目录.md
+++ csharp笔记-000目录.md
@@ -2,11 +2,11 @@
 
 
 ### 前言部分
-1. [csharp笔记-0000目录](csharp笔记-0000目录.md)  
-2. [csharp笔记-000前言](csharp笔记-000前言.md)  
+1. [csharp笔记-000前言](csharp笔记-000前言.md)  
+2. [csharp笔记-000目录](csharp笔记-000目录.md)  
 
 
-### 第 一 部分
+### 第一部分
 3. [csharp笔记-100vscode_cs编程](csharp笔记-100vscode_cs编程.md)  
 4. [csharp笔记-101第一个cs代码](csharp笔记-101第一个cs代码.md)  
 5. [csharp笔记-102字符串入门](csharp笔记-102字符串入门.md)  
@@ -18,3 +18,15 @@
 11. [csharp笔记-108if语句](csharp笔记-108if语句.md)  
 12. [csharp笔记-109数组和foreach](csharp笔记-109数组和foreach.md)  
 13. [csharp笔记-110编程规范](csharp笔记-110编程规范.md)  
+
+
+### 第二部分
+14. [csharp笔记-201代码块和变量范围](csharp笔记-201代码块和变量范围.md)  
+15. [csharp笔记-202swich语句](csharp笔记-202swich语句.md)  
+16. [csharp笔记-203for语句](csharp笔记-203for语句.md)  
+17. [csharp笔记-204while语句](csharp笔记-204while语句.md)  
+18. [csharp笔记-205英雄打怪兽的游戏](csharp笔记-205英雄打怪兽的游戏.md)  
+
+
+### 练习程序列表
+1. [205英雄打怪兽的游戏](205英雄打怪兽的游戏)
